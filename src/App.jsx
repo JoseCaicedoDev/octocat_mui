ff81dcf08c425250e805d6f663955f67d4b6900d
jcaicedo <jose.caicedo@outlook.com>
2023-01-12 12:36:08 -0500

    first commit, added MUI

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material"
+function App() {
+  return (
+    <div>
+      <h1>Octocat</h1>
+      <Button variant="contained">hola</Button>
+    </div>
+  )
+}
+
+export default App
